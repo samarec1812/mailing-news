92be11b53c1175ddefd51b8d5bb9e11b962df3d3
Alexey Kondratev <samarec1812@gmail.com>
2021-06-23 19:07:24 +0300

    Изучение влияния context на timeout

---
+++ client/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var StrOut string
+
+func Client() {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	// url := "https://habr.com/ru"
+	url := "https://google.com"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Request error", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		fmt.Println("Response error", err)
+		StrOut = "ERROR"
+		return
+	}
+	defer resp.Body.Close()
+	var outStr []byte
+	resp.Body.Read(outStr)
+	StrOut = string(outStr)
+
+}
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"./client"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,25 +9,34 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "GET str: %s\n", client.StrOut)
 	fmt.Printf("Served: %s\n", r.Host)
 }
-
+var Sout string
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	 t := time.Now().Format(time.RFC1123)
-	 Body := "The current time is:"
-	 fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", Body)
-	 fmt.Fprintf(w, "<h2 align=\"center\">%s</h2>", t)
-	 fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
-	 fmt.Printf("Served time for: %s\n", r.Host)
+	t := time.Now().Format(time.RFC1123)
+	Body := "The current time is:"
+	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", Body)
+	fmt.Fprintf(w, "<h2 align=\"center\">%s</h2>", t)
+	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "GET str: %s\n", client.StrOut)
+	fmt.Printf("Served time for: %s\n", r.Host)
 }
 
 func main() {
-	http.HandleFunc("/",handler)
+	http.HandleFunc("/", handler)
 	http.HandleFunc("/time", timeHandler)
 
-	err := http.ListenAndServe(":8080", nil)
+
+	for i := 0; i < 80; i++ {
+		go client.Client()
+	}
+
+	err := http.ListenAndServe(":8080",nil)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 }
