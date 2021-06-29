b3f5bda4adebbf304e4487dbd89288018a614512
Alexey Kondratev <samarec1812@gmail.com>
2021-06-29 22:37:38 +0300

    Добавление параметров запроса и работа с хэш-сумой

--- client.go
+++ client.go
@@ -3,54 +3,86 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strconv"
+	"reflect"
 	"time"
 )
 
 type Posts struct {
-	ID string `json:"id"`
+	ID   string `json:"id"`
 	Date string `json:"date"`
 }
+
 // Переделать application/json в другой
 
 type Client struct {
-	BaseURL *url.URL
-	UserAgent string
-
+	BaseURL    *url.URL
+	UserAgent  string
 	httpClient *http.Client
 }
 
+// Структура с параметрами запросов
+type OptionsURL struct {
+	NumLastNews uint
+	Hash string
+	ID string
+}
+
 // Извлекаем методы newRequest(), do() чтобы можно было переиспользовать во всех вызовах API
-func (c *Client) newRequest(method, path, typeRequest string, body interface{}) (*http.Request, error) {
+func (c *Client) newRequest(method, path, typeRequest string, opt OptionsURL, body interface{}) (*http.Request, error) {
 	rel := &url.URL{Path: path}
+
 	u := c.BaseURL.ResolveReference(rel)
 
 	str := fmt.Sprintf("%v", body)
 	request, err := http.NewRequest(method, u.String(), bytes.NewBufferString(str))
+
+	// add request parameters
+	q := url.Values{}
+	// берём имя поля структуры как имя параметра запроса
+	optName := (reflect.Indirect(reflect.ValueOf(opt))).Type().Field(0).Name
+	optName2 := (reflect.Indirect(reflect.ValueOf(opt))).Type().Field(1).Name
+	optName3 := (reflect.Indirect(reflect.ValueOf(opt))).Type().Field(2).Name
+
+	if opt.NumLastNews != 0 {
+		q.Add(optName, fmt.Sprintf("%v", opt.NumLastNews))
+	}
+	if opt.Hash != "" {
+		q.Add(optName2, opt.Hash)
+	}
+	if opt.ID != "" {
+		q.Add(optName3, opt.ID)
+	}
+
+	request.URL.RawQuery = q.Encode()
+	fmt.Println(request.URL.String())
+
 	if err != nil {
 		return nil, err
 	}
 	if body != nil {
 		request.Header.Set("Content-Type", typeRequest)
 	}
-	request.Header.Set("Accept",  typeRequest)
+
+	request.Header.Set("Accept", typeRequest)
 	request.Header.Set("User-Agent", c.UserAgent)
 
 	return request, nil
 
 }
 
-func (c *Client) do(ctx context.Context, request *http.Request, v interface{}) (*http.Response, error) {
+// Вынос метода Do структуры httpClient в отдельную функцию, для переиспользования в других функциях
+func (c *Client) doImplementation(ctx context.Context, request *http.Request, v interface{}) (*http.Response, error) {
 	request = request.WithContext(ctx)
 	resp, err := c.httpClient.Do(request)
 	if err != nil {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			return nil, ctx.Err()
 		default:
 		}
@@ -61,102 +93,125 @@ func (c *Client) do(ctx context.Context, request *http.Request, v interface{}) (
 	// body, err := ioutil.ReadAll(resp.Body)
 	// log.Printf(string(body))
 	// fmt.Println(resp.Body)
-
-	err = json.NewDecoder(resp.Body).Decode(v)
-
+	if resp.Header.Get("Content-Type") == "application/json" {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	} else {
+		_, err = ioutil.ReadAll(resp.Body)
+		// fmt.Println(string(text))
+	}
 	return resp, err
 }
 
-func (c *Client) ListNews(ctx context.Context) ([]Posts, error){
-	request, err := c.newRequest("GET", "/post", "application/json", nil)
+
+
+
+
+// Получение всех новостей
+func (c *Client) GetAllNews(ctx context.Context) ([]Posts, error) {
+	opt := OptionsURL{}
+	request, err := c.newRequest("GET", "/post", "application/json", opt, nil)
 	if err != nil {
 		return nil, err
 	}
 	var posts []Posts
-	_, err = c.do(ctx, request, &posts)
+	_, err = c.doImplementation(ctx, request, &posts)
 	return posts, err
 }
-var ID uint64
 
-// Получение последнего ID
-func (c *Client) GetLastID(ctx context.Context) (string, error) {
-
-	posts, err := c.ListNews(ctx)
-	return posts[len(posts)-1].ID, err
-}
-
-// Добавление новой новости
-func (c *Client) PostNews(ctx context.Context) ([]Posts, error) {
-	lastID, err := c.GetLastID(ctx)
-	strID := ""
-	strDate := ""
-	if err != nil {
-		strID = "0"
-		strDate = time.Now().Format(time.RFC822)
-	} else {
-		num, _ := strconv.ParseUint(lastID, 10, 64)
-		strID = strconv.FormatUint(num + 1, 10)
-		strDate = time.Now().Format(time.RFC822)
-
-	}
-	postBody, _ := json.Marshal(Posts{
-		strID,
-		strDate,
-	})
-	// fmt.Println(string(postBody))
-	responseBody := bytes.NewBuffer((postBody))
-	// fmt.Println(responseBody)
-
-	request, err := c.newRequest("POST", "/post", "application/json", responseBody)
-
+// Получение последних N новостей
+func (c *Client) GetNumLastNews(ctx context.Context, num uint) ([]Posts, error) {
+	opt := OptionsURL{NumLastNews: num}
+	// path := addOptionsURL("/post", OptionsURL{3})
+	request, err := c.newRequest("GET", "/post", "application/json", opt, nil)
 	if err != nil {
 		return nil, err
 	}
-	var post []Posts
-	resp, err := c.do(ctx, request, &post)
-	if resp.StatusCode != 200 {
-		log.Println("HTTP-Error", resp.StatusCode)
-	}
+	var posts []Posts
+	_, err = c.doImplementation(ctx, request, &posts)
+	return posts, err
+}
 
-	return post, err
+// Получение update новости по её ID
+func (c *Client) GetUpdateByID(ctx context.Context, post Posts) (*Posts, error) {
+
+	hash := sha256.New()
+	hashSum := hash.Sum([]byte(post.Date))
+	fmt.Println(string(hashSum))
+	opt := OptionsURL{Hash: string(hashSum), ID: post.ID}
+	request, err := c.newRequest("GET", "/post", "application/json", opt, nil)
+	if err != nil {
+		return nil, err
+	}
+	var posts *Posts
+	_, err = c.doImplementation(ctx, request, &posts)
+	return posts, err
+
+
+
+
+}
+
+var ID uint64
+
+// Получение ID последней новости
+func (c *Client) GetLastID(ctx context.Context) (string, error) {
+
+	posts, err := c.GetAllNews(ctx)
+	return posts[len(posts)-1].ID, err
 }
 
 func NewClient(urlNew string) *Client {
 	newUrl, _ := url.Parse(urlNew)
 	return &Client{
 		httpClient: &http.Client{},
-		BaseURL: newUrl,
-		UserAgent: "my-user-agent",
+		BaseURL:    newUrl,
+		UserAgent:  "my-user-agent",
 	}
 }
+
+
 func main() {
 	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 5 *time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	client := NewClient("http://localhost:8081/")
-	id, err := client.GetLastID(ctx)
-	// news, err := client.PostNews(ctx)
+
+	post, err := client.GetAllNews(ctx)
 	if err == context.DeadlineExceeded {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(id)
-
-	_, err = client.PostNews(ctx)
-	if err == context.DeadlineExceeded {
+	//id, err := client.GetLastID(ctx)
+	//// news, err := client.PostNews(ctx)
+	//if err == context.DeadlineExceeded {
+	//	fmt.Println(err)
+	//	return
+	//}
+	fmt.Println(post)
+	var numLastNews uint
+	_, err = fmt.Scanln(&numLastNews)
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	id, err = client.GetLastID(ctx)
-	// news, err := client.PostNews(ctx)
+	lastNews, err := client.GetNumLastNews(ctx, numLastNews)
 	if err == context.DeadlineExceeded {
 		fmt.Println(err)
 		return
 	}
+	fmt.Println(lastNews)
 
-	fmt.Println(id)
+	// Получение update по ID
+	ID := 5
+	getNews, err := client.GetUpdateByID(ctx, post[ID - 1])
+	if err == context.DeadlineExceeded {
+		fmt.Println(err)
+		return
+	} else if getNews == nil {
+		fmt.Println("Already up to date")
+		return
+	}
+	fmt.Println(getNews)
 
 }
-
-
--- server.go
+++ server.go
@@ -1,54 +1,105 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type postsStruct struct {
-	ID string `json:"id"`
+	ID   string `json:"id"`
 	Date string `json:"date"`
+	Text string `json:"text"`
 }
-
 
 func main() {
 
 	posts := []postsStruct{
-		{"1", "25 Jun 21 19:06 MSK"},
-		{"2", "26 Jun 21 18:01 MSK"},
-		{"3", "27 Jun 21 20:16 MSK"},
+		{"1", "25 Jun 21 19:06 MSK", "Статья 1"},
+		{"2", "26 Jun 21 18:01 MSK", "Статья 2"},
+		{"3", "27 Jun 21 20:16 MSK", "Статья 3"},
+	}
+	for i := 4; i < 20; i++ {
+		posts = append(posts, postsStruct{fmt.Sprintf("%v", i),
+			time.Now().Format(time.RFC822), fmt.Sprintf("Статья %v", i)})
 	}
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// отдаём обычный HTML
 		fileContents, err := ioutil.ReadFile("index.html")
 		if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+			log.Println(err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 		w.Write(fileContents)
 	})
 
-
-
-	http.HandleFunc("/post", func (w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
 		//str := client.Client()
 		//fmt.Fprintln(w, str)
-		fmt.Println("request ", r.URL.Path)
+		fmt.Println("request: ", r.URL.Path)
+		fmt.Println("method request: ", r.Method)
 		defer r.Body.Close()
 
 		// Switch для разных типов запросов
 		switch r.Method {
 		// GET для получения данных
 		case http.MethodGet:
-			productsJson, _ := json.Marshal(posts)
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write(productsJson)
+			r.ParseForm()
+			fmt.Println(r.Form)
+			fmt.Println(r.FormValue("NumLastNews"))
+			if r.FormValue("NumLastNews") == "" && r.FormValue("ID") == "" && r.FormValue("Hash") == "" {
+				productsJson, _ := json.Marshal(posts)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				w.Write(productsJson)
+			} else {
+				if r.FormValue("NumLastNews") != "" {
+					lastNumPost, err := strconv.Atoi(r.FormValue("NumLastNews"))
+					if err != nil {
+						log.Println(err)
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
+					if lastNumPost < 0 || lastNumPost > len(posts) {
+						log.Println(errors.New("number of requested posts is not allowed"))
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
+
+					productsJson, _ := json.Marshal(posts[len(posts)-lastNumPost:])
+					w.Header().Set("Content-Type", "application/json")
+
+					w.WriteHeader(http.StatusOK)
+					w.Write(productsJson)
+				} else if r.FormValue("ID") != "" && r.FormValue("Hash") != "" {
+					ID, err := strconv.Atoi(r.FormValue("ID"))
+					if err != nil {
+						log.Println(err)
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
+					hash := sha256.New()
+					hashSum := hash.Sum([]byte(posts[ID - 1].Date))
+					if string(hashSum) != r.FormValue("Hash") {
+						productsJson, _ := json.Marshal(posts[ID - 1])
+						w.Header().Set("Content-Type", "application/json")
+						w.WriteHeader(http.StatusOK)
+						w.Write(productsJson)
+					} else {
+						w.Header().Set("Content-Type", "text/plain")
+						w.WriteHeader(http.StatusOK)
+						w.Write([]byte("Already up to date"))
+					}
+				}
+			}
 		// POST для добавление чего-то нового
 		case http.MethodPost:
 			decoder := json.NewDecoder(r.Body)
@@ -63,15 +114,12 @@ func main() {
 			}
 			posts = append(posts, news)
 
-
-
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
 	})
 
-
-	err := http.ListenAndServe(":8081",nil)
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		fmt.Println(err)
 		return
